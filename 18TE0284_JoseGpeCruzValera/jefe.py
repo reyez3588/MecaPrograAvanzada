024cd5e66f6596f98b96ee8a76db992ff6b10477
JGKat <cruzvalerajoseguadalupe@gmail.com>
2021-11-10 02:47:23 -0600

    se creo la clase de jefe con mas detalles

--- 18TE0284_JoseGpeCruzValera/jefe.py
+++ 18TE0284_JoseGpeCruzValera/jefe.py
@@ -1,12 +1,32 @@
 """realizare un programa de un jefe que pueda entrevistar a un empleado y conocer su nivel de ingles"""
 
-class Empleado():
-    def __init__(self, nombre, apellido, edad, salario, nivel_ingles):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-        self.salario = salario
-        self.nivel_ingles = nivel_ingles
-    def entrevista(self):
-        print("Hola, mi nombre es {} {} y tengo {} años".format(self.nombre, self.apellido, self.edad))
-        print("Mi salario es de {} y mi nivel de ingles es de {}".format(self.salario, self.nivel_ingles))
+class boss:
+    def __init__(self, name:str , age: int, pay:float, ropa:str, nivel_ingles:int):
+        self.__name = str(name)
+        self.__age = int(age)
+        self.__pay = float(pay)
+        self.__nivel = int(nivel_ingles)  # nivel de ingles superiores a 70 para aceptar
+        self.__nivel_ingles = "nivel alto"
+        self.__vestimenta = str(ropa)
+    try:
+        name = property(lambda self: self.__name)
+        age = property(lambda self: self.__age)
+        pay = property(lambda self: self.__pay)
+        nivel = property(lambda self: self.__nivel)
+    except ValueError:
+        print("error datos erroneos")
+    except TypeError:
+        print("error tipo de datos erroneos")
+    except Exception:
+        print("error desconocido")
+
+    def get_name(self):
+        return self.__name
+
+    def saludo(self):
+        print("hola soy el jefe, mi nombre es", self.__name, "y tengo", self.__age, "años")
+        print("mi sueldo es de", self.__pay, "euros")
+        print("mi nivel de ingles es", self.__nivel_ingles)
+
+    def vestimenta(self):
+        print("mi vestimenta es", self.__vestimenta)
