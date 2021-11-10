43832a3f42f040fad50e3cca1179a21d2ef2a04b
JGKat <cruzvalerajoseguadalupe@gmail.com>
2021-11-10 02:35:38 -0600

    Fixes: se ha agregado encapsulado data pub, prv

--- 18TE0284_JoseGpeCruzValera/empleado.py
+++ 18TE0284_JoseGpeCruzValera/empleado.py
@@ -1,12 +1,16 @@
 """Se realizara una clase de empleado para poder entrevistarlo"""
-
+#aplicamos encapsulamiento
+#como tambien privado y publico de ciertos argumentos
 class Empleado():
     def __init__(self, nombre, apellido, edad, salario, nivel_ingles):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-        self.salario = salario
-        self.nivel_ingles = nivel_ingles
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.__edad = edad
+        self.__salario = salario
+        self.__nivel_ingles = nivel_ingles
+
+    def get_nombre(self):
+        return self.__nombre
     def entrevista(self):
-        print("Hola, mi nombre es {} {} y tengo {} años".format(self.nombre, self.apellido, self.edad))
-        print("Mi salario es de {} y mi nivel de ingles es de {}".format(self.salario, self.nivel_ingles))
+        print("Hola, mi nombre es {} {} y tengo {} años".format(self.__nombre, self.__apellido, self.__edad))
+        print("Mi salario es de {} y mi nivel de ingles es de {}".format(self.__salario, self.__nivel_ingles))
