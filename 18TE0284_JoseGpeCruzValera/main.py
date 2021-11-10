7a5bffb401c62dd8cb7f3a2f66c8e613504d3f9c
JGKat <cruzvalerajoseguadalupe@gmail.com>
2021-11-10 03:45:10 -0600

    changeName  to a Ropa\wearing

--- 18TE0284_JoseGpeCruzValera/main.py
+++ 18TE0284_JoseGpeCruzValera/main.py
@@ -3,6 +3,20 @@ import empleado as emp
 import wearing as ropa
 
 
+def main():
+    print("Bienvenido al programa de gestión de personal")
+    print("¿Qué desea hacer?")
+    print("[1] conocer el jefe")
+    print("[2] verte al espejo")
+    print("[3] conocer el número de empleados")
+    print("[4] conocer la empresa")
+
+    opcion = int(input("Introduzca una opción: "))
+
+    if opcion == 1:
+        Jefe_empresa = jefe.boss("Rodrigo",43,25000,True)
+
+
 
 
 
