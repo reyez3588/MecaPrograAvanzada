882ee5cebdd4c0ceabc28098cd5dc3e26353c4e4
JGKat <cruzvalerajoseguadalupe@gmail.com>
2021-11-10 02:26:30 -0600

    se han agregado archivos necesarios para solucion

---
+++ 18TE0284_JoseGpeCruzValera/jefe.py
@@ -0,0 +1,12 @@
+"""realizare un programa de un jefe que pueda entrevistar a un empleado y conocer su nivel de ingles"""
+
+class Empleado():
+    def __init__(self, nombre, apellido, edad, salario, nivel_ingles):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        self.salario = salario
+        self.nivel_ingles = nivel_ingles
+    def entrevista(self):
+        print("Hola, mi nombre es {} {} y tengo {} años".format(self.nombre, self.apellido, self.edad))
+        print("Mi salario es de {} y mi nivel de ingles es de {}".format(self.salario, self.nivel_ingles))
