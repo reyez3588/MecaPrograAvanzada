aaa53256cf8a79aeee20db9f64f9db9827bba332
JGKat <cruzvalerajoseguadalupe@gmail.com>
2021-11-13 17:54:10 -0600

    Modificcaciones a la vestimenta

--- 18TE0284_JoseGpeCruzValera/Gerente.py
+++ 18TE0284_JoseGpeCruzValera/Gerente.py
@@ -1,5 +1,7 @@
+import Vestimenta
+
 """realizare un programa de un jefe que pueda entrevistar a un empleado y conocer su nivel de ingles"""
-class boss:
+class persona():
     def __init__(self, name:str , age: int, pay:float, ropa:str, nivel_ingles:int, altura:float):
         self,__altura = altura
         self.__name = str(name)
@@ -22,12 +24,20 @@ class boss:
 
     def get_name(self):
         return self.__name
-    def get_altura(self):
-        return self.__altura
+    def get_invitacion(self): # invitacion de empleado a entrevistar
+        #es como un get altura con la condicion de ser una entrevista y conocer la altura
+        print("invitacion de entrevista de empleo")
+        print('\n Correo recibido:\n'
+        'Es de mi agrado informar que se ha agenda su cita con nuestro CEO')
+        print(" El señor ",self.__name,'Es una gran empresario, mide ',self.__altura,
+        'de altura')
+        print("Posee un porte formal por lo que se le recomienda ")
 
-
-
-
+class Gerente(persona):
+    def __init__(self, name:str , age: int, pay:float, ropa:str, nivel_ingles:int, altura:float):
+        super().__init__(name, age, ropa, nivel_ingles, altura)
+        self.__nivel_ingles = "nivel alto"
+        self.__vestimenta = ropa
 
     def saludo(self):
         print("hola soy el jefe, mi nombre es", self.__name, "y tengo", self.__age, "años")
---
+++ 18TE0284_JoseGpeCruzValera/Vestimenta.py
@@ -0,0 +1,95 @@
+"""necesitamos empezar a modificar y crear la base del jefe"""
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#                         -Armario principal-
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+class ropa:
+    def __init__(self, color:str, talla:str, marca:str, genero:str="Unisex"):
+        self.__color = color
+        self.__talla = talla
+        self.__marca = marca
+        self.__genero = genero
+
+    def getterInfo(self):
+        print ("Color: {}\nTalla: {}\nMarca: {}\nGenero: {}"
+        .format(self.__color, self.__talla, self.__marca, self.__genero))
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#                            -SACO-
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+class saco(ropa):
+    def __init__(self, color:str, talla:str, marca:str,
+                genero:str="Unisex", material:str="Formal",botones:int=0):
+        super().__init__(color, talla, marca, genero)
+        self.__color  = color
+        self.__talla  = talla
+        self.__marca  = marca
+        self.__genero = genero
+        self.__material = material
+        self.__botones = botones
+    def verSACO(self):
+        print("Saco color {} de talla {}, Marca: {}".format(self.__color, self.__talla, self.__marca))
+        print("Posee {} botone(s) su saco".format(self.__botones))
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#                            -Pantalon-
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+class pantalon(ropa):
+    def __init__(self, color:str, talla:str, marca:str,
+                genero:str="Hombre"):
+        super().__init__(color, talla, marca)
+        self.__color = color
+        self.__talla = talla
+        self.__marca = marca
+    def verPANTALON(self):
+        print("Pantalon  {} de la {}, marca {}".format(self.__color, self.__talla, self.__marca))
+        print("Talla: {}".format(self.__talla))
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#                            -CAMISETA-
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+class camiseta(ropa):
+    def __init__(self, color:str, talla:str, marca:str,
+                genero:str="Unisex"):
+        super().__init__(color, talla, marca, genero)
+        self.__color = color
+        self.__talla = talla
+        self.__marca = marca
+        self.__genero = genero
+
+    def verCAMISA(self):
+        print("Camisa de {} de {}".format(self.__color, self.__marca))
+        print("Su talla es aprox : {}".format(self.__talla))
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#                            -Zapatos-
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+class zapatos(ropa):
+    def __init__(self, color:str, talla:str, marca:str,
+                genero:str="Unisex"):
+        super().__init__(color, talla, marca)
+    def verZAPATOS(self):
+        print("Zapatos de {} de {}".format(self.__color, self.__marca))
+        print("Talla: {}".format(self.__talla))
+
+pantalonGerente = pantalon("Gris",'40','LV','Hombre')
+zapatosGerente = zapatos("Negros","27","Flexi","Masculino")
+sacoGerente = saco('Azul','Mediana','Rey Palomo','Hombre','Varias telas finas',3)
+camisetaGerente = camiseta('azul','Large','Lacoste')
+
+print()
+# camisetaGerente.getterInfo()
+camisetaGerente.verCAMISA()
+print()
+pantalonGerente.verPANTALON()
+print()
+sacoGerente.verSACO()
+print()
+
+# pantalonGerente.getterInfo()
+# zapatosGerente.getterInfo()
